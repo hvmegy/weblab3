dbf3efa88519a0fca619e2a2da1696fee967bfc3
hvmegy <hamegy08lleo@gmail.com>
2024-11-20 11:33:05 +0700

    init

---
+++ src/main/java/hcmute/com/SecurityDemo/service/impl/UserServiceImpl.java
@@ -0,0 +1,27 @@
+package hcmute.com.SecurityDemo.service.impl;
+
+import hcmute.com.SecurityDemo.entity.Users;
+import hcmute.com.SecurityDemo.repo.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class    UserServiceImpl implements UserDetailsService {
+
+        @Autowired
+        UserRepository userRepository;
+
+        @Override
+        public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                System.out.println(username);
+                Users user = userRepository.getUserByUsername(username);
+                if (user == null){
+                        throw new UsernameNotFoundException("Could not find user");
+                }
+
+                return new MyUserService(user);
+        }
+}
